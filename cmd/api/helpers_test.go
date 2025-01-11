aab867100c04a75cd20d7f6fec2440b1ffd4ff83
Tim Engle <timothy.englew@gmail.com>
2025-01-11 15:53:57 -0800

    refactor: refactor application struct methods

--- cmd/api/helpers_test.go
+++ cmd/api/helpers_test.go
@@ -1,8 +1,92 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"log/slog"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateApplication(t *testing.T) {
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+
+	cfg := &config{}
+
+	app, err := createApplication(logger, cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, app)
+	assert.Equal(t, logger, app.logger)
+	assert.Equal(t, cfg, app.cfg)
+}
+
+func TestInitializeServer(t *testing.T) {
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+	cfg := &config{port: "8080"}
+	app := &application{logger: logger, cfg: cfg}
+
+	server := app.intializeServer()
+
+	assert.NotNil(t, server)
+	assert.Equal(t, ":8080", server.Addr)
+	assert.NotNil(t, server.Handler)
+	assert.Equal(t, time.Minute, server.IdleTimeout)
+	assert.Equal(t, 5*time.Second, server.ReadTimeout)
+	assert.Equal(t, 10*time.Second, server.WriteTimeout)
+	assert.NotNil(t, server.ErrorLog)
+}
+
+func TestCreateLogger(t *testing.T) {
+	logger := createLogger()
+
+	assert.NotNil(t, logger)
+	_, ok := logger.Handler().(*slog.TextHandler)
+	assert.True(t, ok)
+}
 
 func TestWriteJSON(t *testing.T) {
 	t.Run("Test WriteJSON", func(t *testing.T) {
 	})
+}
+
+func TestGetEnv(t *testing.T) {
+	tests := []struct {
+		name         string
+		key          string
+		value        string
+		defaultValue string
+		keyExists    bool
+	}{
+		{
+			name:         "value for key is defined",
+			key:          "PORT",
+			value:        "8080",
+			defaultValue: "0000",
+			keyExists:    true,
+		},
+		{
+			name:         "value for key is undefined",
+			key:          "PORT",
+			value:        "",
+			defaultValue: "0000",
+			keyExists:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv(tt.key, tt.value)
+			defer os.Unsetenv(tt.key)
+
+			v := getEnv(tt.key, tt.defaultValue)
+
+			if !tt.keyExists {
+				assert.Equal(t, tt.defaultValue, v)
+				return
+			}
+
+			assert.Equal(t, tt.value, v)
+		})
+	}
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,21 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"log/slog"
-	"net/http"
-	"os"
-	"time"
 )
 
 const (
 	VERSION_ENV = "HIVEBOX_VERSION"
 	PORT_ENV    = "HIVEBOX_PORT"
 )
-
-type application struct {
-	cfg    *config
-	logger *slog.Logger
-}
 
 type config struct {
 	port    string
@@ -39,40 +30,4 @@ func main() {
 	svr := app.intializeServer()
 
 	log.Fatal(svr.ListenAndServe())
-}
-
-// Initialize the HTTP server with configuration settings
-func (app *application) intializeServer() *http.Server {
-	return &http.Server{
-		Addr:         ":" + app.cfg.port,
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
-	}
-}
-
-func createLogger() *slog.Logger {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	return logger
-}
-
-func createApplication(
-	logger *slog.Logger,
-	cfg *config,
-) (*application, error) {
-	app := &application{
-		logger: logger,
-		cfg:    cfg,
-	}
-	return app, nil
-}
-
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-	return value
 }
